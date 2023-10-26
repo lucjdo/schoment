34c5357d3569e1a139eb7e61a95e43cf66c59607
lucjdo <luciano.sejudo@gmail.com>
2023-10-26 23:05:23 +0200

    feat: add eslint react plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
