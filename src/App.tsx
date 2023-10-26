2213437c6db53c20471c7ce9a20bfa481e9363e2
lucjdo <luciano.sejudo@gmail.com>
2023-10-27 01:46:32 +0200

    feat: styling config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Menu from '@containers/Menu'
+import { Box, Grid } from '@mui/material';
 import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>School challenge</h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <Menu />
+        </Grid>
+        <Grid item xs={10}>
+        </Grid>
+      </Grid>
+    </Box>
     </div>
   )
 }
