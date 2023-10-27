a776f62cce34bbfa9b291eb28298dc21aa0ef6e7
lucjdo <luciano.sejudo@gmail.com>
2023-10-27 16:03:11 +0200

    feat: add rooms mock

---
+++ src/containers/Rooms/index.tsx
@@ -0,0 +1,5 @@
+import ROOMS from '../../mocks/Rooms.json'
+
+export default function Rooms() {
+  return <div>{JSON.stringify(ROOMS)}</div>
+}
--- src/containers/Routes/index.tsx
+++ src/containers/Routes/index.tsx
@@ -1,9 +1,12 @@
+import Rooms from '@containers/Rooms'
 import { Route, Switch } from 'wouter'
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path='/rooms'>Rooms</Route>
+      <Route path='/rooms'>
+        <Rooms />
+      </Route>
       <Route path='/rooms/:id'>Room</Route>
       <Route path='/students'>Students</Route>
     </Switch>
