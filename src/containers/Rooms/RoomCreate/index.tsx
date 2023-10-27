5c6c561b8e442641600c8abd0eef3751a4a873f1
lucjdo <luciano.sejudo@gmail.com>
2023-10-27 20:24:51 +0200

    feat: create new room form

--- src/containers/Rooms/RoomCreate/index.tsx
+++ src/containers/Rooms/RoomCreate/index.tsx
@@ -1,10 +1,59 @@
 import ViewLayout from '@components/ViewLayout'
-import { Typography } from '@mui/material'
+import {
+  FormControl,
+  FormLabel,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  TextField,
+  Select,
+  Stack,
+  Typography
+} from '@mui/material'
+import { AMENITIES_OPTIONS } from './utils'
 
 export default function RoomCreate() {
   return (
     <ViewLayout>
-      <Typography variant='h4'>New Room</Typography>
+      <Typography variant='h4' mb={3}>
+        New Room
+      </Typography>
+      <form>
+        <Stack></Stack>
+        <Stack spacing={3} width={500}>
+          <FormControl>
+            <FormLabel>Name</FormLabel>
+            <TextField placeholder='Type in here…' defaultValue='Classroom' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Location</FormLabel>
+            <TextField placeholder='Type in here…' defaultValue='Build 1' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Capacity</FormLabel>
+            <TextField
+              placeholder='Type in here…'
+              defaultValue={30}
+              type='number'
+            />
+          </FormControl>
+          <FormControl sx={{ m: 1 }}>
+            <FormLabel id='demo-multiple-name-label'>Amenities</FormLabel>
+            <Select
+              labelId='demo-multiple-name-label'
+              id='demo-multiple-name'
+              value={[]}
+              multiple
+            >
+              {AMENITIES_OPTIONS.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Stack>
+      </form>
     </ViewLayout>
   )
 }
