4152a885e74a33a2e998c44e53aa7b773bf860bd
lucjdo <33455403+lucjdo@users.noreply.github.com>
2023-10-27 10:45:14 -0300

    feat: create app main layout (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { Route } from 'wouter'
-import { Box, Grid, Paper } from '@mui/material'
+import MainLayout from '@components/MainLayout'
 import Menu from '@containers/Menu'
+import Routes from '@containers/Routes'
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={2}>
-            <Menu />
-          </Grid>
-          <Grid item xs={10}>
-            <Paper sx={{ height: 'calc(100vh - 90px)', background: '#fafafa' }}>
-              <Route path='/rooms'>Rooms</Route>
-              <Route path='/rooms/:id'>Room</Route>
-              <Route path='/students'>Students</Route>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Box>
-    </div>
+    <MainLayout>
+      <Menu />
+      <Routes />
+    </MainLayout>
   )
 }
 
