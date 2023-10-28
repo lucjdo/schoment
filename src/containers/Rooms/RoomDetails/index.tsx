b5df02d82562993ba84c75fc56b080fa743f7238
lucjdo <luciano.sejudo@gmail.com>
2023-10-28 20:45:55 +0200

    fetch room from hook

--- src/containers/Rooms/RoomDetails/index.tsx
+++ src/containers/Rooms/RoomDetails/index.tsx
@@ -4,8 +4,30 @@ import { useRoom } from '@hooks/rooms/useRoom'
 import { Stack, Typography } from '@mui/material'
 import { RouteComponentProps } from 'wouter'
 
+function Detail({
+  property,
+  value
+}: {
+  property: string
+  value: string | number | string[]
+}) {
+  return (
+    <Stack>
+      <Typography variant='subtitle1' sx={{ color: 'info.dark' }}>
+        {property}
+      </Typography>
+      <Typography variant='h5' sx={{ fontFamily: 'Open Sans' }}>
+        {value}
+      </Typography>
+    </Stack>
+  )
+}
+
 export default function RoomDetails({ params }: RouteComponentProps) {
   const { data, isLoading } = useRoom(params.id!)
+
+  if (!data) return
+
   return (
     <ViewLayout>
       <Stack gap={2}>
@@ -13,9 +35,18 @@ export default function RoomDetails({ params }: RouteComponentProps) {
         {isLoading ? (
           'Loading'
         ) : (
-          <Stack>
-            <Stack>
-              <Typography>Name</Typography>
+          <Stack direction='row' justifyContent='space-between'>
+            <Stack gap={4}>
+              <Detail property='Name' value={data.name} />
+              <Detail property='Type' value={data.type} />
+            </Stack>
+            <Stack gap={4}>
+              <Detail property='Location' value={data.location} />
+              <Detail property='Capacity' value={data.capacity} />
+            </Stack>
+            <Stack gap={4}>
+              <Detail property='Furniture' value={data.furniture} />
+              <Detail property='Amenities' value={data.amenities} />
             </Stack>
           </Stack>
         )}
