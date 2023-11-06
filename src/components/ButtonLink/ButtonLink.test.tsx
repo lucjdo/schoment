8df441defa3ec9fbd9649329e8741551cf5dcc21
lucjdo <luciano.sejudo@gmail.com>
2023-11-06 19:56:16 +0100

    add button link tests

--- src/components/ButtonLink/ButtonLink.test.tsx
+++ src/components/ButtonLink/ButtonLink.test.tsx
@@ -1,5 +1,18 @@
+import ButtonLink from '.'
+import { render, screen, fireEvent } from '@testing-library/react'
+
 describe('Button Link', () => {
-  test('should render the component', () => {
-    expect(1 + 1).toBe(2)
+  beforeEach(() => {
+    render(<ButtonLink href='/go-to'>Go to</ButtonLink>)
+  })
+
+  test('should render the content', () => {
+    expect(screen.getByText('Go to')).toBeDefined()
+  })
+
+  test('should navigate on click', () => {
+    const link = screen.getByRole('link')
+    fireEvent.click(link)
+    expect(location.href).toMatch('/go-to')
   })
 })
