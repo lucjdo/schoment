e7136350969cbdf7344d1aa90612267e91365ef6
lucjdo <luciano.sejudo@gmail.com>
2023-11-06 19:24:48 +0100

    install vitest

---
+++ src/components/ButtonLink/ButtonLink.test.tsx
@@ -0,0 +1,5 @@
+describe('Button Link', () => {
+  test('should render the component', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: { environment: 'jsdom', globals: true },
   resolve: {
     alias: {
       '@components': '/src/components',
